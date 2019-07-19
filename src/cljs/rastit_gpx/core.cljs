7c17b5685dda408f9492c5030f1215d6236fa843
Aki Kaivola <aki.kaivola@gmail.com>
2019-07-19 21:49:20 +0300

    Add helper subs for xhr processing

--- src/cljs/rastit_gpx/core.cljs
+++ src/cljs/rastit_gpx/core.cljs
@@ -1,14 +1,17 @@
 (ns rastit-gpx.core
   (:require
    [reagent.core :as reagent]
+   [re-frame.core :as re-frame]
    [rastit-gpx.events :as events]
    [rastit-gpx.subs]
    [rastit-gpx.config :as config]
-   [re-frame.core :as re-frame]))
+   [rastit-gpx.rastit :refer [available-rastit-routes]]))
 
 (defn ui []
   [:div
-   [:p "Hello Devcards"]])
+   [:h1 "Rastit.fi GPX route downloader"]
+   [:a {:href "#" :on-click #(js/fetchGpx "9717" "test_9717")} "test"]
+   [available-rastit-routes]])
 
 (defn dev-setup []
   (when config/debug?
--- src/cljs/rastit_gpx/subs.cljs
+++ src/cljs/rastit_gpx/subs.cljs
@@ -1,7 +1,21 @@
 (ns rastit-gpx.subs
   (:require
    [re-frame.core :as r]
+   [rastit-gpx.ajax :as ajax]
    [reagent.ratom :as ratom]))
+
+(defn- query-ajax [db request]
+  (ajax/http-op (merge
+                 request
+                 {:handler
+                  (fn [response]
+                    (let [processed ((or (:handler request) identity) response)]
+                      (r/dispatch-sync
+                       [:set-db [:http request] processed])
+                      processed))}))
+  (ratom/make-reaction
+   (fn []
+     (get-in @db [:http request]))))
 
 (r/reg-sub
  :query-db
@@ -9,3 +23,19 @@
    (if (sequential? path)
      (get-in db path)
      (get db path))))
+
+(r/reg-sub-raw
+ :http
+ (fn [db [_ request]]
+   (query-ajax db request)))
+
+(r/reg-sub-raw
+ :http-rastit
+ (fn [db [_ request]]
+   (query-ajax
+    db
+    (update
+     request
+     :url
+     (fn [url]
+       (str "https://cors-anywhere.herokuapp.com/https://www.rastit.fi" url))))))
